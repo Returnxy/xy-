f862a9eff9f0ad6c8e5d82de9fdee912cf77001f
Returnxy <115648907+Returnxy@users.noreply.github.com>
2022-10-13 16:40:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,108 +1,8 @@
-/**
-  * Edit features in './message/msg.js'
-  * Contact me on WhatsApp wa.me/628990999600
-  * Follow https://github.com/rtwone
-  * If you want to buy an updated script that is not encrypted, please WhatsApp me
-*/
-
-"use strict";
-const {
-	default: makeWASocket,
-	BufferJSON,
-	initInMemoryKeyStore,
-	DisconnectReason,
-	AnyMessageContent,
-        makeInMemoryStore,
-	useSingleFileAuthState,
-	delay
-} = require("@adiwajshing/baileys")
-const figlet = require("figlet");
-const fs = require("fs");
-const moment = require('moment')
-const chalk = require('chalk')
-const logg = require('pino')
-const clui = require('clui')
-const { Spinner } = clui
-const { serialize } = require("./lib/myfunc");
-const { color, mylog, infolog } = require("./lib/color");
-const time = moment(new Date()).format('HH:mm:ss DD/MM/YYYY')
-let setting = JSON.parse(fs.readFileSync('./config.json'));
-let session = `./${setting.sessionName}.json`
-let welcome = JSON.parse(fs.readFileSync('./database/welcome.json'));
-
-function title() {
-      console.clear()
-	  console.log(chalk.bold.green(figlet.textSync('WaBot MD', {
-		font: 'Standard',
-		horizontalLayout: 'default',
-		verticalLayout: 'default',
-		width: 80,
-		whitespaceBreak: false
-	})))
-	console.log(chalk.yellow(`\n                        ${chalk.yellow('[ Created By Irfan ]')}\n\n${chalk.red('Chitanda Eru Bot')} : ${chalk.white('WhatsApp Bot Multi Device')}\n${chalk.red('Follow Insta Dev')} : ${chalk.white('@irfann._x')}\n${chalk.red('Message Me On WhatsApp')} : ${chalk.white('+62 857-9145-8996')}\n${chalk.red('Donate')} : ${chalk.white('085791458996 ( Ovo/Pulsa/Dana )')}\n`))
-}
-
-/**
-* Uncache if there is file change;
-* @param {string} module Module name or path;
-* @param {function} cb <optional> ;
-*/
-function nocache(module, cb = () => { }) {
-	console.log(`Module ${module} sedang diperhatikan terhadap perubahan`) 
-	fs.watchFile(require.resolve(module), async () => {
-		await uncache(require.resolve(module))
-		cb(module)
-	})
-}
-/**
-* Uncache a module
-* @param {string} module Module name or path;
-*/
-function uncache(module = '.') {
-	return new Promise((resolve, reject) => {
-		try {
-			delete require.cache[require.resolve(module)]
-			resolve()
-		} catch (e) {
-			reject(e)
-		}
-	})
-}
-
-const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
-const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
-const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
-
-const store = makeInMemoryStore({ logger: logg().child({ level: 'fatal', stream: 'store' }) })
-
-async function fanStart() {
-const connectToWhatsApp = async () => {
-	const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
-	const conn = makeWASocket({
-            printQRInTerminal: true,
-            logger: logg({ level: 'fatal' }),
-            auth: state,
-            browser: ["Chitanda Eru Multi Device", "Safari", "3.0"],
-	    getMessage: async key => {
-              return {
-                
-              }
-          }
-        })
-	title()
-        store.bind(conn.ev)
-	
-	/* Auto Update */
-	require('./message/help')
-	require('./lib/myfunc')
-	require('./message/msg')
-	nocache('./message/help', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
-	nocache('./lib/myfunc', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
-	nocache('./message/msg', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
-	
+@@ -103,6 +103,7 @@ const connectToWhatsApp = async () => {
 	conn.multi = true
 	conn.nopref = false
 	conn.prefa = 'anjing'
+	conn.mode = 'public'
 	conn.ev.on('messages.upsert', async m => {
 		if (!m.messages) return;
 		var msg = m.messages[0]
@@ -111,15 +11,15 @@ const connectToWhatsApp = async () => {
 		require('./message/msg')(conn, msg, m, setting, store, welcome)
 	})
 	conn.ev.on('connection.update', (update) => {
+          if (global.qr !== update.qr) {
+           global.qr = update.qr
+          }
           const { connection, lastDisconnect } = update
-            if (connection === 'close') { 
-	     status.stop()
-		    reconnect.stop()
-		    starting.stop()
-		    console.log(mylog('server ready'))
-                lastDisconnect.error?output?statusCode !== DisconnectReason.loggedOut ? connectToWhatsApp() : console.log(mylog('connection logged out...'))
-	    }, })
-	conn.ev.on('creds.update', () => saveState)
+            if (connection === 'close') {
+                lastDisconnect.error?output?statusCode !== DisconnectReason.loggedOut ? connectToWhatsApp() : console.log('connection logged out...')
+            }
+        })
+	conn.ev.on('creds.update', await saveCreds)
 	
         conn.ev.on('group-participants.update', async (data) => {
           const isWelcome = welcome.includes(data.id) ? true : false
@@ -142,14 +42,10 @@ const connectToWhatsApp = async () => {
             }
           }
         })
-
 	conn.reply = (from, content, msg) => conn.sendMessage(from, { text: content }, { quoted: msg })
-
 	return conn
 }
-
 connectToWhatsApp()
 .catch(err => console.log(err))
 }
-
 fanStart()
